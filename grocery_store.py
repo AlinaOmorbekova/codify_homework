27415e0c3baac09911f3ccf34152fd98fcb9f21c
Alina <46772913+AlinaOmorbekova@users.noreply.github.com>
2021-08-11 12:25:29 +0600

    Add files via upload

---
+++ grocery_store.py
@@ -0,0 +1,29 @@
+#В магазине есть список разных продуктов, у каждого продукта есть 
+# название, цена, уникальный номер. Сперва пользователю нужно отобразить
+# весь список продуктов с их информацией, после нужно сказать чтобы он 
+# ввел название товара, если такой товар есть предложить пользователю 
+# купить этот товар, и ввести сумму если введенная сумма меньше цены 
+# которая указана на товар то нужно уведомить его что у вас не хватает 
+# денег чтобы купить, иначе сказать ему что вы получили товар.
+def grocery_store(list_of_groceries, list_of_prices):
+    list_of_groceries = ('apples', 'bread', 'ramen', 'strawberries')
+    list_of_prices = (150, 80, 240, 450)
+    print(list_of_groceries)
+    print(list_of_prices)
+    inpt = input('Введите название товара: ')
+    for i in list_of_groceries:
+        if inpt in list_of_groceries:
+            indx = list_of_groceries.index(inpt)
+            print('Хотите совершить покупку?')
+            money = int(input('Введите сумму: '))
+            cost_of_apple = int(list_of_prices[indx])
+            if money >= int(cost_of_apple):
+                print('Вы успешно совершили покупку!')
+                break
+            else:
+                print('У вас не хватает средств!')
+
+list_of_groceries = ('apples', 'bread', 'ramen', 'strawberries')
+list_of_prices = (150, 80, 240, 450) 
+grocery_store(list_of_groceries, list_of_prices)    
+
