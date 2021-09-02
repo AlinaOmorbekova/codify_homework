e500d1385a56a4e3fd16a79a460fb5cc297c83ef
Alina <46772913+AlinaOmorbekova@users.noreply.github.com>
2021-09-02 23:42:59 +0600

    Create vehicle.py

---
+++ OOP/vehicle.py
@@ -0,0 +1,10 @@
+#1. Создайте класс Vehicle с атрибутами экземпляра max_speed и mileage
+class Vehicle:
+    max_speed = 100
+    mileage = 200
+print('max speed is', Vehicle.max_speed) 
+print('mileage is', Vehicle.mileage)  
+
+#2. Создайте класс Vehicle без переменных и методов
+class Vehicle:
+    pass
